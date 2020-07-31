6ce6fa43e0d0b5b557b39fa5e03702883ec30854
Krith J <pheekaythailand@gmail.com>
2020-07-31 17:06:35 +0700

    3107

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
 
+import java.util.Arrays;
 
 class Main{
     public static void main(String[] args){
-        String x = "Good vibes" ;
-        System.out.print(x);
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+       
+        System.out.println( Arrays.toString(numbers));
     }
 }
